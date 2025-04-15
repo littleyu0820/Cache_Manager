bc0832a3f1f85a99b9e6d9c545136fb832b55bf1
LiuYun0820 <qw22033@icloud.com>
2025-04-15 17:30:36 +0800

    Create Main_SystemV5.h

---
+++ Cache_Manager/Main_SystemV5.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_SYSTEM_H
+#define MAIN_SYSTEM_H
+
+#include <string>
+#include <map>
+#include <windows.h>
+#include <vector>
+#include <QStringList>
+
+std::wstring GetActiveProcessName();
+std::wstring GetProcessUserName();
+std::string WStringToString(const std::wstring&);
+std::wstring StringToWString(const std::string&);
+void SaveData_Json(const std::map<std::wstring, unsigned int>&);
+void SaveData_Path(const std::map<std::wstring, std::wstring>&);
+std::wstring Use_Path(const std::wstring&);
+std::map<std::wstring, unsigned int> LoadUsageDataFromJson();
+std::map<std::wstring, std::wstring> LoadPathDataFromJson();
+std::vector<std::pair<std::wstring, unsigned int>> AppsRank(const std::map<std::wstring, unsigned int>&);
+std::wstring GetPath();
+void SaveCustomData_Json(const std::map<std::wstring, std::wstring>&);
+std::map<std::wstring, std::wstring> LoadCustomDataFromJson();
+#endif // PROCESSTRACKER_H
